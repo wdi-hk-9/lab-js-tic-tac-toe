944dc61a7f0614ee125cc510a854b4fa51863f51
adaau <ada.au.cheng@gmail.com>
2015-10-04 22:03:09 +0800

    javascript works, but cant connect DOM

--- scripts/app.js
+++ scripts/app.js
@@ -1,82 +1,115 @@
-window.onload() {
-/*
-when player clicks one of the 9 divs: 
-register in JS that it has been clicked
-[check that it is free
-if free, ]return an "x" in that div
-[if not free, alert "that square is not free. please choose another."]
+window.onload = function() {
+var gameboard = [null, null, null,
+				  null, null, null,
+				  null, null, null];
 
-computer then goes and puts an "o" in an unoccupied div
-then player clicks an unoccupied div
-computer then goes and puts an "x" in an unoccupied div
-then player clicks on unoccupied div
-check for winner
-if winner, alert and stop play
-if no winner, continue play. as before, and check after each move.
-until all divs are filled. then end game.
-*/
+var moves;
 
-/* Get Player Move */
-function clicked(id) {
-  document.getElementById(id).addEventListener("click", writeX);
+function setMark(player, position) {
+	gameboard[position] = player;
 }
 
-function writeX(id) {
-  document.getElementbyId(id).innerHtml("x");
+function getMarkAt(position) {
+	return gameboard[position];
 }
 
-/* Get Computer Move */
+function getPersonMove() {
+	var personInput = prompt("Please choose a number between 0 to 8, inclusive");
+	if (gameboard[personInput] === null) {
+		setMark("x", personInput);
+	}
+	else {
+		getPersonMove();
+	}
+}
+
 function getComputerMove() {
-  var randomIndex = Math.round(Math.random() * 9);
-  document.getElementById("square"+randomIndex).innerHtml("o");
+	var randomIndex = Math.floor(Math.random() * 9);
+	if (gameboard[randomIndex] === null) {
+		setMark("o", randomIndex);
+	}
+	else {
+		getComputerMove();
+	}
 }
 
-/* Check for winner
-get all three if three in a row OR three in a column OR three in diagonal
-three in a row if all x's or all o's
-three in a column if ditto
-three in diagonal if ditto
-declare a winner
-*/
-
-function getWinner() {
-  if (winnerIs("x")) {
-	return "x";  
-  }
-  else if (winnerIs("o")) {
-	return "o";
-  }
-  else {
-	return null;
-  }
+function checkForWinner() {
+	if (winnerIs("x")) {
+		return "x";
+	}
+	else if (winnerIs("o")) {
+		return "o";
+	}
+	else {
+		return null;
+	}
 }
 
 function winnerIs(player) {
-	return winsRow(player) || winsColumn(player) || winsDiag(player);
+	return winsRow(player) || winsCol(player) || winsDiag(player);
 }
 
 function winsRow(player) {
-  return allThree(player, "square0", "square1", "square2") || 
-  allThree(player, "square3", "square4", "square5") || 
-  allThree(player, "square6", "square7", "square8");
-}
-/* 
-function winsColumn(player) {
-  return allThree(player, sq("0"), sq("3"), sq("6")) || 
-  allThree(player, sq("1"), sq("4"), sq("7")) || 
-  allThree(player, sq("2"), sq("5"), sq("8"));	
+	return allThree(player, getMarkAt(0), getMarkAt(1), getMarkAt(2)) ||
+	allThree(player, getMarkAt(3), getMarkAt(4), getMarkAt(5)) ||
+	allThree(player, getMarkAt(6), getMarkAt(7), getMarkAt(8)); 
 }
 
-function winsDiag() {
-  return allThree(player, sq("0"), sq("4"), sq("8")) || 
-  allThree(player, sq("2"), sq("4"), sq("6"));
-}
-*/
-function allThree(player, sqA, sqB, sqC) {
-	return (sqA === player) && (sqB === player) && (sqC === player);
+function winsCol(player) {
+	return allThree(player, getMarkAt(0), getMarkAt(3), getMarkAt(6)) ||
+	allThree(player, getMarkAt(1), getMarkAt(4), getMarkAt(7)) ||
+	allThree(player, getMarkAt(2), getMarkAt(5), getMarkAt(8)); 
 }
 
+function winsDiag(player) {
+	return allThree(player, getMarkAt(0), getMarkAt(4), getMarkAt(8)) ||
+	allThree(player, getMarkAt(2), getMarkAt(4), getMarkAt(6));
+}
 
+function allThree(player, cellOne, cellTwo, cellThree) {
+	return (cellOne === player) && (cellTwo === player) && (cellThree === player);
+}
 
+function addXOrO() {
+	for(moves = 0; moves <=8; moves++) {
+		if (moves % 2 === 0) {
+			getPersonMove();
+			console.log(gameboard);
+		
+			var winner = checkForWinner();
+			if (winner) {
+				alert(winner + " won!");
+				resetBoard();
+			}
+		}
+		
+		else {
+			getComputerMove();
+			
+			console.log(gameboard);
+			var winner = checkForWinner();
+			if (winner) {
+				alert(winner + " won!");
+				resetBoard();
+			}
+		}
+	}
+	
+	if (moves >= 9) {
+		alert("Game Over");
+		return;
+	}	
+}
 
+function resetBoard() {
+	for (var i = 0; i < gameboard.length; i++) {
+		gameboard[i] = null;
+	}
+}
+
+document.getElementById("reset-button").addEventListener("click", resetBoard);
+
+for (var i = 0; i < gameboard.length; i++) {
+	document.getElementById("sq[i]").addEventListener("click", addXOrO);
+}
 }
